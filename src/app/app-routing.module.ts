04a26cf157e79051b59d062fc4df4d73104348af
smita <smita@DESKTOP-4JUNKNU>
2024-09-30 22:57:00 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { RegistrationListComponent } from './registration-list/registration-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"register",pathMatch:'full'},
+  {path:"register",component:CreateRegistrationComponent},
+  {path:'list',component:RegistrationListComponent},
+  {path:'detail/:gymRegisteredUser_pk',component:UserDetailComponent},
+  {path:'update/:gymRegisteredUser_pk',component:CreateRegistrationComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
